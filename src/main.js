d610c4d7bc7682705f415a4ad5fad3127fd843c5
Gustavo B <gustavobc2001@hotmail.com>
2025-09-19 07:13:01 -0300

    0.90.2 release

--- src/main.js
+++ src/main.js
@@ -3881,8 +3881,7 @@ function buildOverlayMain() {
           }).buildElement()
         .buildElement()
         .addDiv({'style': 'position: absolute; left: 0; bottom: 2px; text-align: left; padding: 0; pointer-events: auto; user-select: text; line-height: 12px;'}).
-          addSmall({'textContent': 'Made by SwingTheVine | Fork Seris0', 'style': 'color: #94a3b8; font-size: 0.74em; opacity: 0.85;'}).buildElement()
-        .buildElement()
+        addSmall({'textContent': `Made by SwingTheVine | Fork Seris0 | v${version}`, 'style': 'color: #94a3b8; font-size: 0.74em; opacity: 0.85;'}).buildElement()        .buildElement()
       .buildElement()
     .buildElement()
   .buildOverlay(document.body);
